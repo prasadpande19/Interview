325b4edf8f40f0ee7cb1df8263ce1b7a20d73614
prasad <prasad@192.168.1.139>
2022-05-23 21:56:36 +0100

    Initial Commit pushed

---
+++ automateFirst/src/com/automateFirst/testCase.java
@@ -0,0 +1,11 @@
+package com.automateFirst;
+
+public class testCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello this is first commit");
+
+	}
+
+}
